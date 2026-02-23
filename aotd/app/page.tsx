c50eb4c322cec6aac1db64b02c220dc36606c712
Marc Wijesuriya <102212857+MarcWije@users.noreply.github.com>
2026-02-23 12:08:02 +0530

    Added spotify player

--- aotd/app/page.tsx
+++ aotd/app/page.tsx
@@ -1,17 +1,27 @@
 "use client"
 
-import Image from "next/image";
 import { belanosima, bellota, baskervville, jost, rajdhani } from '@/app/ui/fonts';
 import BlurText from "@/components/BlurText";
 import Grainient from "@/components/Grainient";
 import CurvedLoop from "@/components/CurvedLoop";
 import ClickSpark from "@/components/ClickSpark";
 import TiltedCard from '@/components/TiltedCard';
+import Stack from "@/components/Stack";
+import SpotifyPopup from '@/components/SpotifyPopUp';
 
 export default function Home() {
   const handleAnimationComplete = () => {
     console.log('Animation completed!');
   };
+
+  const images = [
+  "/fear-inoculum.jpg",
+  "/metropolis-pt-2.jpg",
+  "/ghost-reveries.jpg",
+  "/magma.jpg"
+];
+
+  const albumId = "4v6kYkn7mYjlWS9vONmmCP";
 
   return (
     
@@ -26,8 +36,8 @@ export default function Home() {
       <div style={{ width: '100%', height: '100%', position: 'fixed', zIndex: -1 }}>
         <Grainient
           color1="#000009"
-          color2="#16006f"
-          color3="#212763"
+          color2="#184054"
+          color3="#245F7C"
           timeSpeed={0.6}
           colorBalance={-0.14}
           warpStrength={1}
@@ -63,8 +73,8 @@ export default function Home() {
             Don't know what to listen to today? We're here to help
           </p>
         </div>
-        <div className="flex gap-4 max-w-4xl ">
-          <div className="p-3">
+        <div className="flex gap-4 w-10/12 mx-auto ">
+          <div className="p-6">
             <TiltedCard
               imageSrc="/octavarium.jpg"
               altText="Dream Theater - Octavarium Album Cover"
@@ -79,25 +89,53 @@ export default function Home() {
             />
           </div>
           <div className="flex flex-col items-start space-y-3 justify-center">
-            <br></br>
               <h2 className={`${jost.className} text-6xl font-bold text-gray-200`}>Octavarium</h2>
               <h3 className={`${jost.className} text-5xl font-normal text-gray-500`}>Dream Theater</h3>
               <h3 className={`${rajdhani.className} text-5xl font-extralight text-gray-500`}>2005</h3>
           </div>
+          <div className="flex flex-col justify-center items-center ml-auto">
+            <h3 className={`${jost.className} text-4xl font-light text-gray-400`}>Similar Albums:</h3> 
+            <div style={{ width: 300, height: 300 }}>
+              <Stack
+                randomRotation={false}
+                sensitivity={200}
+                sendToBackOnClick={true}
+                cards={images.map((src, i) => (
+                  <img 
+                    key={i} 
+                    src={src} 
+                    alt={`card-${i + 1}`} 
+                    style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+                  />
+                ))}
+                autoplay={false}
+                autoplayDelay={3000}
+                pauseOnHover={false}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex p-3 justify-center">
+                <SpotifyPopup albumId={`${albumId}`}/>
         </div>
         <div className="flex flex-col items-center">
           <p className={`${bellota.className} text-4xl font-bold text-center text-gray-500 p-2`}>
             Octavarium
           </p>
-          <p className={`${baskervville.className} text-xl text-left text-gray-300 max-w-3/6 leading-7`}><i>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at pellentesque urna, ut lacinia urna. In tristique arcu in eros maximus, rhoncus convallis velit ultricies. Fusce laoreet libero et urna maximus tincidunt. Suspendisse potenti. Nullam in massa sit amet augue auctor volutpat. Duis venenatis tincidunt massa, eget malesuada leo tempor nec. Cras ac ligula elementum, rhoncus turpis eu, tempus ex. Quisque scelerisque dolor orci, id faucibus risus venenatis eget. Sed eget ullamcorper nisi, vitae luctus velit. Etiam mi magna, porta sit amet auctor id, interdum sit amet massa. Aliquam aliquet congue nisl nec ornare. Etiam feugiat nunc quis ex gravida, molestie egestas leo consectetur. Aliquam et dapibus libero. Fusce imperdiet sollicitudin mauris, quis faucibus neque sodales non. Ut maximus lobortis nisl a vestibulum.
-            <br></br>
-            <br></br>
-            Curabitur a quam nibh. Nunc porta congue tortor in maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum vel risus dignissim, aliquam sapien quis, pulvinar justo. Mauris efficitur justo non risus ullamcorper, in ultricies enim sollicitudin. Donec sit amet eros id magna lobortis ultrices. Nulla sed nisi tellus. Vivamus finibus neque at rhoncus gravida. Maecenas eros metus, ultrices sed elit in, interdum ultricies ante. Nunc nisl arcu, rutrum vel nisl et, rutrum gravida orci. Proin sit amet sapien sed felis finibus dignissim. Sed viverra posuere hendrerit. Sed sit amet ligula pretium, efficitur tellus in, laoreet ante. Quisque a molestie lectus. Nulla porta, mauris convallis varius hendrerit, diam arcu fermentum massa, tempor ornare lorem mi quis risus.
-            <br></br>
-            <br></br>
-            Etiam molestie leo a libero venenatis lacinia. In velit augue, sodales nec neque quis, aliquet fermentum augue. Praesent dolor lectus, sagittis id tortor eget, vulputate sagittis turpis. Pellentesque maximus, dolor id tincidunt viverra, est ipsum imperdiet lacus, et sodales urna neque a tortor. Morbi leo massa, pharetra at mauris in, consequat bibendum mi. Donec molestie, ligula ac bibendum tincidunt, velit eros rhoncus lectus, non aliquam mi augue ac ante. Etiam vitae tempus velit, eget interdum dui. Duis nec metus sed libero pharetra varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec nec urna turpis. Praesent tincidunt tempor porta. Fusce ex nibh, eleifend sit amet tincidunt vel, pharetra et mauris. Phasellus interdum malesuada aliquet. Ut laoreet dolor eros. Integer nec nunc vestibulum nunc congue hendrerit. 
-          </i>
+          <p className={`${baskervville.className} text-xl text-left text-gray-300 max-w-3/6 leading-7`}>
+            Dream Theater returns with their eighth studio album and in typical DT fashion, the latest release bears little resemblance to the album that came directly before it. Train of Thought was "too metal" for some fans (but not for me) and chock full of arguably over the top soloing that sometimes worked against the compositions. Those that found some of Train of Thought to be overkill should be delighted by Octavarium. Case in point: John Petrucci plays with far more restraint while Jordan Rudess' keyboards are higher up in the mix.
+            <br></br><br></br> 
+            At 75 minutes, Octavarium is a very long CD. Dream Theater always cram a lot of music onto their albums and I long ago accepted the fact that their CDs can be a bit of an endurance test. But the time factor is more noticeable with Octavarium because it gets off to such a painfully sluggish start. Before anyone starts to think I'm being too harsh, bear with me here: Octavarium contains some of the band's best ever material. But the first few songs are just so ordinary by Dream Theater standards in a "been there done that" sort of way that one might be tempted to hit the stop button before hearing the frankly outstanding compositions yet to come.
+            <br></br><br></br>
+            "The Root of All Evil" continues the saga of Mike Portnoy's alleged experience with The Twelve Step Program that began on Six Degrees of Inner Turbulence ("Glass Prison") and continued with Train of Thought ("This Dying Soul"). Those were fantastic pieces of music while "The Root of All Evil" is just anticlimactic somehow. Even more problematic is "The Answer Lies Within", a complete throwaway ballad that is inappropriately sequenced as track two of the CD. Things start looking up with "These Walls" and the curveball that is the U2 pop flavored "I Walk Beside You"; seriously, James LaBrie appears to be emulating Bono on the chorus.
+            <br></br><br></br>
+            By the time of "Panic Attack", I'm getting hooked. The intensity and heaviness reaches that of some of the better moments of Train of Thought. Really, from "Panic Attack" forward, Octavarium is consistently, well�awesome. "Never Enough" is another killer heavy track before the band pulls in the reigns for the ten minute "Sacrificed Sons", a haunting epic that conjures up images of 9/11, dogmatic politicians and the futility of religious wars. The lengthy instrumental break is simply ferocious.
+            <br></br><br></br>
+            But the magnum opus here is without question the twenty four minute title track, a wonderful symphonic rock tour de force that gets my vote as Dream Theater's finest epic ever. Compared to most of the rest of the CD, "Octavarium" isn't in the least bit heavy and it isn't until the twelve minute mark that the musical fireworks begin. Jordan Rudess really gets a chance to shine here as he never has before, with a fine Rick Wakeman like synth solo which in turn shifts over to a neat Genesis inspired instrumental arrangement. In case anyone doubts the band's prog rock influences, check out the "Full Circle" section of the title track for some amusing stream of conscious lyrics mentioning some famous epics! "Octavarium" finally concludes with a recapitulation of the main theme featuring an orchestra and simply defines the term symphonic rock.
+            <br></br><br></br>
+            So where does Octavarium sit among Dream Theater's fairly prolific back catalog? I'll leave that one up to the reader to decide. For my taste, it's the band's finest album since Scenes From a Memory; it's certainly their most tuneful album in quite some time. I oftentimes struggle with star ratings but I'm giving Octavarium four strong stars. I would easily give it higher marks if the first few tracks were up to snuff, but they are just so bland that five stars would be an inaccurate representation of the album in balance. But program out at least the first two songs and you have one of the best albums of 2005. 
+            <br></br><br></br>
+            <i>This album review placeholder was taken from https://www.seaoftranquility.org/</i>
           </p>
         </div>
         <CurvedLoop 
---
+++ aotd/components/SpotifyPopUp.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { belanosima } from '@/app/ui/fonts';
+import { FaSpotify } from "react-icons/fa";
+
+interface SpotifyPopupProps {
+  albumId: string;
+}
+
+export default function SpotifyPopup({ albumId }: SpotifyPopupProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      {/* Trigger button (optional) */}
+      {!open && (
+        <button
+          onClick={() => setOpen(true)}
+          className={`${belanosima.className} fixed bottom-4 right-4 bg-black text-green-600 px-4 py-2 rounded shadow-lg z-50 flex items-center gap-2`}
+        >
+          <FaSpotify size={20} /> Play Album
+        </button>
+      )}
+
+      {/* Fixed popup */}
+      {open && (
+        <div className="fixed bottom-4 right-4 w-[320px] h-100 z-50 shadow-lg rounded overflow-hidden flex flex-col bg-gray-900">
+          {/* Close button */}
+          <button
+            onClick={() => setOpen(false)}
+            className="absolute top-1 left-1/2 -translate-x-1/2 text-white text-xl font-bold z-50"
+          >
+            ×
+          </button>
+
+          {/* Spotify iframe */}
+          <iframe
+            src={`https://open.spotify.com/embed/album/${albumId}`}
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allow="encrypted-media"
+            className="rounded"
+          ></iframe>
+        </div>
+      )}
+    </>
+  );
+}
