5502daefad1254db4f949f20dab773afe066bc78
Marc Wijesuriya <102212857+MarcWije@users.noreply.github.com>
2026-03-04 17:38:13 +0530

    Changed random albums to dynamic

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import AlbumPage from '@/components/AlbumPage';
 import { randomAlbum } from '@/lib/getAlbums';
 
--- lib/getAlbums.ts
+++ lib/getAlbums.ts
@@ -7,8 +7,6 @@ import getMap from "./HashMap";
 
 const albumsDirectory = path.join(process.cwd(), "components/albums");
 
-export const dynamic = "force-dynamic";
-
 export type AlbumData = {
   albumId: string;
   heading: string;
